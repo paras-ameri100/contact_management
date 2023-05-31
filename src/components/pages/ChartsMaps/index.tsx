cd7d6b6aafac0db1dd09393e16491fe957d636db
paras-ameri100 <paras.bhatt@ameri100.com>
2023-05-31 12:14:02 +0530

    Contact Page

---
+++ src/components/pages/ChartsMaps/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageTitle from '../../helpers/PageTitle'
+
+export default function ChartsMaps() {
+  return (
+    <>
+      <PageTitle title='Charts & Maps' />
+    </>
+  )
+}
+
